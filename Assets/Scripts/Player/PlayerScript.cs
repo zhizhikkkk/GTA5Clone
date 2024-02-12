31f5d1ff05151594af3b8f5c5d54fe63dc538737
zhizhikkkk <zhanelmamyt@gmail.com>
2024-02-12 14:12:46 +0600

    Player Movement | Jump | Sprint

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -6,27 +6,75 @@ public class PlayerScript : MonoBehaviour
 {
     [Header("Player Movement")]
     public float playerSpeed = 1.1f;
+    public float playerSprint = 5f;
 
     [Header("Player Animator & Gravity")]
     public CharacterController cC;
+    public float gravity = -9.81f;
+
+    [Header("Player Script Camera")]
+    public Transform playerCamera;
+
+
+    [Header("Player Jumping & Velocity")]
+    public float jumpRange = 1f;
+    public float turnCalmTime = 0.1f;
+    float turnCalmVelocity;
+    Vector3 velocity;
+    public Transform surfaceCheck;
+    bool onSurface;
+    public float surfaceDistance = 0.4f;
+    public LayerMask surfaceMask;
 
     private void Update()
     {
-        PlayerMove();
-    }
+        onSurface = Physics.CheckSphere(surfaceCheck.position, surfaceDistance);
 
-    private void  PlayerMove()
-    {
-        float horizontalAxis = Input.GetAxisRaw("Horizontal");
-        float verticalAxis = Input.GetAxisRaw("Vertical");
-
-        Vector3 direction = new Vector3(horizontalAxis,0f, verticalAxis).normalized;
-        
-        if (direction.magnitude >= 0.1f)
+        if(onSurface && velocity.y<0)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
-            cC.Move(direction.normalized * playerSpeed * Time.deltaTime);
+            velocity.y = -2f;
         }
 
+        //gravity 
+        velocity.y += gravity * Time.deltaTime;
+        cC.Move(velocity * Time.deltaTime);
+        playerMove(playerSpeed);
+        Jump();
+        Sprint();
+    }
+
+    void playerMove(float speed)
+    {
+        float horizontal_axis = Input.GetAxisRaw("Horizontal");
+        float vertical_axis = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal_axis, 0f, vertical_axis).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            cC.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+
+    private void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && onSurface)
+        {
+            velocity.y = Mathf.Sqrt(jumpRange * -2 * gravity);
+        }
+        
+    }
+
+    private void Sprint()
+    {
+        if (Input.GetButton("Sprint") && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) && onSurface)
+        {
+            playerMove(playerSprint);
+        }
     }
 }
